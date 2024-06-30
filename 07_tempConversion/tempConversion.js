ba2775a768d0451599ceca08061db348fe8494aa
amothm80 <amo.1980@gmail.com>
2024-07-01 00:57:19 +0300

    completed till exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  celsius = ((fahrenheit - 32) * 5) / 9;
+  if (celsius != 0) {
+    return Number(celsius.toFixed(1));
+  } else {
+    return 0;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celcius) {
+  fahrenheit = (celcius * 9) / 5 + 32;
+  if (fahrenheit != 0) {
+    return Number(fahrenheit.toFixed(1));
+  } else {
+    return 0;
+  }
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
