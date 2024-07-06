ccf38f656d8352802ba8636166c20c78498a2b54
amothm80 <amo.1980@gmail.com>
2024-07-06 23:21:06 +0300

    completed 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const peopleAges = people.map(person=>{
+        if(!person[`yearOfDeath`]){
+            person['age'] = new Date().getFullYear() - person.yearOfBirth;
+        }else{
+            person['age'] = person.yearOfDeath - person.yearOfBirth;
+        }
+        return person
+    })
+    peopleAgesSorted = peopleAges.sort((firstPerson, secondPerson)=>firstPerson.age > secondPerson.age ? -1:1);
+    return peopleAgesSorted[0]
 };
 
 // Do not edit below this line
