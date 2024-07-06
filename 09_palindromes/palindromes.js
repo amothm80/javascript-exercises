c03c919d506674cec8fa9ec7a169a53afe889df1
amothm80 <amo.1980@gmail.com>
2024-07-06 12:46:06 +0300

    completed exercise 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (sentence) {
+    pal1 = sentence.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
+    pal2 = pal1.split("").reverse().join("")
+    return pal1 == pal2;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  if (n < 0) {
+    return "OOPS";
+  }
+  return Math.round(
+    (((1 + Math.sqrt(5)) / 2) ** n - ((1 - Math.sqrt(5)) / 2) ** n) /
+      Math.sqrt(5)
+  );
 };
 
 // Do not edit below this line
